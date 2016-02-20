c66822d25aaac6a7e38e25e3c0a28b13f00764de
davygora <glory-gerrard@ukr.net>
2016-02-20 19:25:58 +0200

    Create method work! & refactoring code

--- homework_3.rb
+++ homework_3.rb
@@ -1,4 +1,3 @@
-#main class
 class Developer
 
   def initialize(dev_name)
@@ -6,18 +5,24 @@ class Developer
     @task_list = []
   end
 
+
   def add_task(task_name)
-  	@task_list.push(task_name)
-  	puts %Q{#{@dev_name}: добавлена задача "#{task_name}".Всего в списке задач: #{@task_list.count}}
+    @task_list.push(task_name)
+    puts %Q{#{@dev_name}: добавлена задача "#{task_name}".Всего в списке задач: #{@task_list.count}}
   end
 
   def tasks
-  	@task_list.map.with_index{ |a, i| "#{i+1}. #{a}"}
+    @task_list.map.with_index{ |a, i| "#{i+1}. #{a}"}
   end
 
+  def work!
+    puts %Q{#{@dev_name}: выполнена задача "#{@task_list.shift}". Осталось задач: #{@task_list.count}}
+  end
 
 end
 
-#dev = Developer.new('Vasya')
-#dev.add_task('Programming')
-puts dev.tasks
+dev = Developer.new('Vasya')
+dev.add_task('Programming')
+# puts dev.tasks
+dev.work!
+dev.work!
