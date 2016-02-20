7b9c619f8a24bcb36dc59b307106918b43340a05
davygora <glory-gerrard@ukr.net>
2016-02-20 04:11:37 +0200

    Create class developer and methods add_task, tasks

---
+++ homework_3.rb
@@ -0,0 +1,23 @@
+#main class
+class Developer
+
+  def initialize(dev_name)
+    @dev_name = dev_name
+    @task_list = []
+  end
+
+  def add_task(task_name)
+  	@task_list.push(task_name)
+  	puts %Q{#{@dev_name}: добавлена задача "#{task_name}".Всего в списке задач: #{@task_list.count}}
+  end
+
+  def tasks
+  	@task_list.map.with_index{ |a, i| "#{i+1}. #{a}"}
+  end
+
+
+end
+
+#dev = Developer.new('Vasya')
+#dev.add_task('Programming')
+#puts dev.tasks
