4f55d4e29ee14511e14c5d4684375a99ff27f5a5
davygora <glory-gerrard@ukr.net>
2016-02-29 22:33:10 +0200

    Create some methods..

--- homework_3.rb
+++ homework_3.rb
@@ -9,10 +9,8 @@ class Developer
   end
 
   def add_task task_name
+    raise 'Слишком много работы!' if @task_list.count > self.class::MAX_TASKS
     @task_list.push task_name
-    if @task_list.count > self.class::MAX_TASKS
-      raise 'Слишком много работы!'
-    end
     puts %Q{#{@dev_name}: добавлена задача "#{task_name}".Всего в списке задач: #{@task_list.count}}
   end
 
@@ -42,6 +40,10 @@ class Developer
   def can_work?
     @task_list.count > 0
   end
+
+  def dev_type
+    :developer
+  end
 end
 
 class JuniorDeveloper < Developer
@@ -50,13 +52,17 @@ class JuniorDeveloper < Developer
   MAX_LENGTH = 20
 
   def add_task task_name
-    raise 'Слишком сложно!' if task_name.length > MAX_LENGTH      
+    raise 'Слишком сложно!' if task_name.length > MAX_LENGTH
     super
   end
 
   def work!
     raise 'Нечего делать!' if @task_list.empty?
     puts %Q{#{@dev_name}: пытаюсь делать задачу "#{@task_list.shift}". Осталось задач: #{@task_list.count}}
+  end
+
+  def dev_type
+    :junior
   end
 end
 
@@ -73,46 +79,67 @@ class SeniorDeveloper < Developer
       puts 'Что-то лень'
     end
   end
+
+  def dev_type
+    :senior
+  end
 end
 
 class Team
 
-  attr_reader :seniors, :juniors, :developers
+  attr_reader :seniors, :juniors, :developers, :team_array
 
   def initialize(&block)
-    @team = []
+    @team_array = []
+    @seniors    = []
+    @developers = []
+    @juniors    = []
+    @filtres    = {}
     instance_eval &block
   end
 
   def have_seniors(*dev_names)
-    @seniors = dev_names
-    @seniors.map{|i| make_developer(SeniorDeveloper, i)}
+    dev_names.each{|i| @seniors.push(make_developer(SeniorDeveloper, i))}
+    @team_array.push(@seniors)
   end
 
   def have_developers(*dev_names)
-    @developers = dev_names
-    @developers.map{|i| make_developer(Developer, i)}
+    dev_names.each{|i| @developers.push(make_developer(Developer, i))}
+    @team_array.concat(@developers)
   end
 
   def have_juniors(*dev_names)
-    @juniors = dev_names
-    @juniors.map{|i| make_developer(JuniorDeveloper, i)}
+    dev_names.each{|i| @juniors.push(make_developer(JuniorDeveloper, i))}
+    @team_array.concat(@juniors)
   end
 
-   def make_developer(type, dev_name)
-     @team.push(type.new(dev_name))
-   end
+  def make_developer(type, dev_name)
+     type.new(dev_name)
+  end
+
+  def on_task(name, &block)
+    @filtres[name] = block
+  end
+
+  def add_task(task)
+
+  end
+
+  def all
+    @team_array
+  end
+
+  def priority(*list)
+    @priority = list
+  end
 
 end
 
 team = Team.new do
 
-  have_seniors 'oleg', 'oksana' 
+  have_seniors 'Олег', 'Оксана' 
   have_developers "Олеся", "Василий", "Игорь-Богдан"
   have_juniors "Владислава", "Аркадий", "Рамеш"
-  @team.each{|i| puts i}
+
+  priority :juniors, :developers, :seniors
 end
- pp team
-# pp team.seniors
-# pp team.juniors
-# pp team.developers
